5cc01c5c215d188ecd40f00ff5a2aa05ee56679b
jiyeon1117 <s2019w24@e-mirim.hs.kr>
2021-06-10 10:05:27 +0900

    몽고DB 연동

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express'); //express 모듈 가져오기
 const app = express();  
 const port = 3000;  //포트번호 설정
 
+const mongoose = require('mongoose');  //MongoDB 모듈 가져오기
+//MongoDB 연결
+mongoose.connect('클러스터 생성 시 넣을 예정', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))  //연결 확인
+  .catch(err => console.log(err)) //에러 확인 가능
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 })
