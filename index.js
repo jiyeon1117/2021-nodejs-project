4657efff4b98c9a316a745590be5b1443f906c90
jiyeon1117 <s2019w24@e-mirim.hs.kr>
2021-06-17 10:11:23 +0900

    회원가입 판별 로직 작성

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require('express'); //express 모듈 가져오기
 const app = express();  
 const port = 3000;  //포트번호 설정
-
 const mongoose = require('mongoose');  //MongoDB 모듈 가져오기
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+//body-parser 옵션
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 //MongoDB 연결
 mongoose.connect('mongodb+srv://jiyeon:0317@cluster.wiunt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))  //연결 확인
   .catch(err => console.log(err)) //에러 확인 가능
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/', (req, res) => res.send('Hello World!'))
+
+//회원가입
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
